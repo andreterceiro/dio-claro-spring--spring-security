5e7ec706d606246fcb78205cb76a0b81ecc4f78e
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-10-28 12:41:57 -0300

    feat: adding an UserRepository repository

---
+++ src/main/java/dio/spring/security/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package dio.spring.security.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import dio.spring.security.models.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("SELECT e FROM User e JOIN FETCH e.roles WHERE e.username= (:username)")
+    public User findByUsername(@Param("username") String username);
+}
